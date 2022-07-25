500077b459889ef40bf35454da3ca6b4e8d365b6
Thanakorn  Watthanaparueda <idvrchat@gmail.com>
2022-07-25 13:33:21 +0700

    แก้ไขระบบ Authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,8 @@
 
                     <p><h4><b>Name : </b> {{ Auth::user()->name }} </h4></p>
                     <p><h4><b>Email : </b> {{ Auth::user()->email }} </h4></p>
+                    <p><h4><b>Address : </b> {{ Auth::user()->address }} </h4></p>
+                    <p><h4><b>Phone : </b> {{ Auth::user()->phone }} </h4></p>
                 </div>
             </div>
         </div>
